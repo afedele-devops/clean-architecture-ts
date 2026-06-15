30dac290510cbf3c6d0d8f37ce01a23ed5d782de
Antonio Fedele <antonio.fedele.dev@gmail.com>
2026-06-15 12:15:26 -0300

    Estructura de carpetas en Clean Architecture

--- main.ts
+++ main.ts
@@ -1,4 +1,10 @@
-import {checkHealth} from './src/shared/health';
+import { buildServer } from "@infrastructure/http/server"
 
-const healthStatus = checkHealth();
-console.log(`Health Status: ${healthStatus.status}, Timestamp: ${healthStatus.timestamp}`); 
+const port = Number(process.env.PORT ?? 3000)  
+buildServer().then(app => {
+    app.listen(port, () => {
+        console.log(`Server running on port ${port}`)
+    })
+}).catch(err => {
+    console.error("Error starting server:", err)
+})  
